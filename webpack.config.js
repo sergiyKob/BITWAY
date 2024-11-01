8da4187ab5b6388bfd63c74f24cf354101e0e736
sergiyKob <sergiykob799@gmail.com>
2024-11-01 21:27:01 +0200

    add agents-page

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const config = {
     mode: 'production',
     entry: {
         index: './src/js/index.js',
-        about: './src/js/about.js'
-        // contacts: './src/js/contacts.js',
+        about: './src/js/about.js',
+        agents: './src/js/agents.js'
     },
     output: {
         filename: '[name].bundle.js',
