f9eebf7f499efe31a33cb6076fa1cbddb904fde8
sergiyKob <sergiykob799@gmail.com>
2024-11-01 22:00:16 +0200

    add burger script, remove visivility-hidden in video__link

--- src/js/about.js
+++ src/js/about.js
@@ -1,6 +1,6 @@
 
-// import mobileNav from './modules/mobile-nav.js';
-// mobileNav();
+import mobileNav from './modules/mobile-nav.js';
+mobileNav();
 
 
 // import slider from './modules/slider.js';
